4a00a89d42926cd9fbf1516e28d6c58e956c848e
ziataro <mitriotik@yandex.ru>
2021-11-19 17:34:37 +0300

    oval

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('generate_oval.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawEllipse(50, 50, 50, 50)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
